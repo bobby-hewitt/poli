86dd24dcd725c691005e2b3a9c9dbd1bff256075
Robert Hewitt <>
2020-06-09 00:37:32 +0100

    initial

---
+++ build/precache-manifest.f2c7bf7a25b4318f8c4da6d2015e7a62.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6414c64c45fce09d8b1a1f5c34fc6361",
+    "url": "/index.html"
+  },
+  {
+    "revision": "821b2754ccbc74312d24",
+    "url": "/static/css/main.7af77ca0.chunk.css"
+  },
+  {
+    "revision": "95f7741cbf536bd0dca5",
+    "url": "/static/js/2.3a7aa456.chunk.js"
+  },
+  {
+    "revision": "821b2754ccbc74312d24",
+    "url": "/static/js/main.5b39a4af.chunk.js"
+  },
+  {
+    "revision": "66b27179a952c2faad14",
+    "url": "/static/js/runtime-main.cf09ae57.js"
+  },
+  {
+    "revision": "6a1af9a16bcc4ca04adde8198f02172c",
+    "url": "/static/media/arrow.6a1af9a1.svg"
+  },
+  {
+    "revision": "c702dc1e3509a5b6a9efa4306e1c9faa",
+    "url": "/static/media/govt.c702dc1e.svg"
+  },
+  {
+    "revision": "dece2b40534a2fa0384a5cb4ef38b43a",
+    "url": "/static/media/members.dece2b40.svg"
+  },
+  {
+    "revision": "2caa15836c0b7ed334ff2c680b558ed7",
+    "url": "/static/media/new.2caa1583.svg"
+  },
+  {
+    "revision": "d31bc9349df4f70cd8139fe33996c9f2",
+    "url": "/static/media/news.d31bc934.svg"
+  },
+  {
+    "revision": "13e1259d0dfa7d2fc7777348d2e40419",
+    "url": "/static/media/post.13e1259d.svg"
+  },
+  {
+    "revision": "4add1a52900c20ea825bfcb334e0836e",
+    "url": "/static/media/social.4add1a52.svg"
+  }
+]);
---
+++ src/index.js
@@ -0,0 +1,122 @@
+const express =  require('express')
+const cors = require('cors')
+const request = require('request')
+const app = express()
+const Feed = require('rss-to-json');
+var bodyParser = require('body-parser')
+var path = require('path')
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(express.static(__dirname + '/node_modules'));
+app.use(express.static(path.resolve(__dirname, '..', 'build')));  
+app.get('/api/', function(req, res,next) {  
+    res.sendFile(__dirname + '/index.html');
+});
+const PORT = process.env.PORT || 9000
+require('dotenv').config({path: '.env'})
+
+
+app.get('/api/votingrecord', (req, res) => {
+	
+	request(`https://commonsvotes-api.parliament.uk/data/divisions.json/membervoting?queryParameters.memberId=${req.query.member}`, (err, response, body) => {
+		if (err) return 
+		res.send(body)
+	})
+})
+
+
+app.get('/api/member', (req, res) => {
+	request(`https://members-api.parliament.uk/api/Members/Search?Name=${req.query.name}&skip=0&take=20`, (err, response, body) => {
+		res.send(JSON.parse(body))
+	})
+})
+
+
+app.get('/api/newssources', (req, res) => {
+	request(`https://newsapi.org/v2/sources?country=gb&apiKey=${process.env.NEWS_KEY}`, (err, response, body) => {
+		res.send(JSON.parse(body))
+	})
+})
+
+app.get('/api/news', (req, res) => {
+	let url = req.query.s ? 
+	`https://newsapi.org/v2/everything?q=${req.query.s}&sortBy=popularity&pageSize=100&apiKey=${process.env.NEWS_KEY}`:
+	`https://newsapi.org/v2/top-headlines?country=gb&pageSize=100&apiKey=${process.env.NEWS_KEY}`
+	request(url, (err, response, body) => {
+		if (err) return 
+		else{
+			
+			res.send(JSON.parse(body))
+		}
+	})
+})
+
+app.get('/api/postcode', (req, res) => {
+	request(`https://api.postcodes.io/postcodes/${req.query.postcode}`, (err, response, body) => {
+		body = JSON.parse(body)
+		if (body.result.parliamentary_constituency){
+			request(`https://members-api.parliament.uk/api/Location/Constituency/Search?searchText=${body.result.parliamentary_constituency}`, (err2, response2, body2) => {
+				res.send(JSON.parse(body2))
+			})
+		}
+		
+	})
+})
+
+
+app.get('/api/divisions', (req, res) => {
+	
+	let now = new Date()
+	// request(`https://commonsvotes-api.parliament.uk/data/divisions.json/search?queryParameters.endDate=${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate() + 1}`, (err, response, body) => {
+	request(`https://commonsvotes-api.parliament.uk/data/divisions.json/groupedByParty?queryParameters.endDate=${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate() + 1}`, (err, response, body) => {
+
+		
+		res.send(JSON.parse(body))
+	})
+
+})
+
+
+app.get('/api/bills', (req, res) => {
+	Feed.load('https://services.parliament.uk/Bills/AllPublicBills.rss', function(err, rss){
+   		res.send(JSON.parse(rss.items));
+	});
+})
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+});
+
+
+
+// request(`https://api.postcodes.io/postcodes/${'e13at'}`, (err, response, body) => {
+// 		body = JSON.parse(body)
+// 		if (body.result.parliamentary_constituency){
+// 			request(`https://members-api.parliament.uk/api/Location/Constituency/Search?searchText=${body.result.parliamentary_constituency}`, (err2, response2, body2) => {
+// 				let a = JSON.parse(body2)
+// 				
+// 				let newResponse = {
+// 					constituency: a.items[0].name,
+// 					memberFrom: a.items[0].startDate,
+// 					memberTo: a.items[0].endDat
+					
+// 					member: {
+// 						value: a.items[0].value.currentRepresentation.value,
+						
+// 					},
+// 				}
+// 				
+// 				// res.send(JSON.parse(body2))
+// 			})
+// 		}
+		
+// 	})
+
+
+
+
+
+app.listen(PORT, () =>{
+	
+})
